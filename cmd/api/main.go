6b2cd150c6a24e0fd199374d9ce2b288ba37667f
Francisco <bonand04@gmail.com>
2024-04-02 22:24:45 -0300

    implement server graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,16 +3,45 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 	"transaction-routine/internal/clock"
 	"transaction-routine/internal/server"
 )
 
 func main() {
-	ctx := context.Background()
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
 	cl := clock.New()
-	server := server.NewServer(ctx, cl)
+	server := server.NewServer(serverCtx, cl)
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalf("cannot start server: %s", err)
+	// Graceful shutdown
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	go func() {
+		<-sig
+		shutdownCtx, cancel := context.WithTimeout(serverCtx, 30*time.Second)
+		defer cancel()
+
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				log.Fatal("graceful shutdown timed out.. forcing exit.")
+			}
+		}()
+
+		err := server.Shutdown(shutdownCtx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		serverStopCtx()
+	}()
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("server terminated with error: %s", err)
 	}
+
+	<-serverCtx.Done()
 }
